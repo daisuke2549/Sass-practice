148211f98e813d2de36aebe03714a79d0f3b96e4
Daisuke Sasaki <gerund25495641@gmail.com>
2021-04-17 08:16:01 +0900

    add javascript file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let score = 70;
+
+console.log(score);
+
+function average(a, b){
+     return  (a+b)/2;
+}
+
+average(30, 70);
+
+
+const fruits = ['grape', 'banana', 'lemon', 'apple'];
+console.log(fruits[1]);
